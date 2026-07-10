2011d1f16b8b2af874c2a63ccf349e613ee15243
ARUN L <108601293+Arun100203@users.noreply.github.com>
2026-07-10 21:51:49 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for i in strs:
+            res += f"{i} \n"
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        print(s)
+        return s.split(" \n")[0:-1]
