70b959cee091bbef18a23118464a4b544624876a
ARUN L <108601293+Arun100203@users.noreply.github.com>
2026-08-25 17:44:38 +0530

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.py
@@ -0,0 +1,68 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        temp = head
+
+        # head, tail = self.reverse(head)
+        pre_head = pre_tail = None
+        ans = ListNode(-1)
+        res = ans
+        while temp:
+            temp_head = temp
+            n = 1
+            while n < k and temp.next:
+                temp = temp.next
+                n += 1
+            if n < k or temp == None:
+                break
+            
+            pre_temp = temp
+            temp = temp.next if temp else None
+            # print(n, temp and temp.val, pre_temp and pre_temp.val)
+            pre_temp.next = None if pre_temp else None
+            cur_head, cur_tail = self.reverse(temp_head)
+            # print(cur_head.val, cur_tail.val)
+
+            ans.next = cur_head
+            ans = cur_tail
+
+        print(n, ans.val, temp and temp.val, temp_head.val, "val")
+
+        # while res:
+        #     print(res.val)
+        #     res = res.next
+
+        # while ans:
+        #     print(ans.val)
+        #     ans = ans.next
+
+        # while temp_head:
+        #     print(temp_head.val)
+        #     temp_head = temp_head.next
+
+        if n < k and temp != None:
+            ans.next = temp_head
+
+        return res.next
+
+            
+
+    def reverse(self, head):
+        pre = None
+        cur = head
+        end = head
+        while cur:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+
+        return pre, end
+
+                
